d0c95929d6740f130102cc7e81c6b8e654bd7a3b
Nia <nia.miteva@gmail.com>
2024-05-27 10:48:49 +0300

    Added the header for the database-related services

---
+++ Database-Management-System/Database-Management-System/DatabaseService.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Database.h"
+
+class DatabaseService
+{
+private:
+	Database database;
+	std::string file;
+	bool is_opened;
+
+	DatabaseService();
+
+    void addcolumn(const std::string& table_name, const std::string& column_name, const std::string& column_type);
+    void aggregate(const std::string& table_name, size_t search_column, const std::string& search_value, size_t target_column, const std::string& operation);
+    void close();
+    void count(const std::string& table_name, size_t search_column, const std::string& search_value);
+    void deleteRows(const std::string& table_name, size_t search_column, const std::string& search_value);
+    void describe(const std::string& name);
+    void exportTable(const std::string& name, const std::string& file_name);
+    void help() const noexcept;
+    void innerjoin(const std::string& table1, size_t column1, const std::string& table2, size_t column2);
+    void insert(const std::vector<std::string>& values_to_insert);
+    void open(const std::string& file_name);
+    void print(const std::string& name);
+    void rename(const std::string& old_name, const std::string& new_name);
+    void save();
+    void saveas(const std::string& file_name);
+    void saveTo(const std::string& file_name);
+    void select(size_t column, const std::string& value, const std::string& table);
+    void showtables() const;
+    void update(const std::string& table, size_t search_column, const std::string& search_value, size_t target_column, const std::string& target_value);
+
+
+    double sum(const Table& table, const std::string& value, size_t searchColumn, size_t targetColumn) const;
+    double product(const Table& table, const std::string& value, size_t searchColumn, size_t targetColumn) const;
+    double maximum(const Table& table, const std::string& value, size_t searchColumn, size_t targetColumn) const;
+    double minimum(const Table& table, const std::string& value, size_t searchColumn, size_t targetColumn) const;
+
+    void visualizationInfo() const;
+
+public:
+    static DatabaseService& getInstance();
+    void run();
+
+    DatabaseService(DatabaseService const&) = delete;
+    DatabaseService& operator=(DatabaseService const&) = delete;
+};
